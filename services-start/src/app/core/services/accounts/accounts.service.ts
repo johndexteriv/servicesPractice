cddcd1f154270211a4682ff3ea228e9a7a57b8d7
John Garner <jdextergarneriv@gmail.com>
2021-03-08 14:21:31 -0500

    create accounts service to hold accounts array, remove accounts array from app.ts

---
+++ services-start/src/app/core/services/accounts/accounts.service.ts
@@ -0,0 +1,16 @@
+export class AccountsService {
+  accounts = [
+    {
+      name: "Master Account",
+      status: "active",
+    },
+    {
+      name: "Testaccount",
+      status: "inactive",
+    },
+    {
+      name: "Hidden Account",
+      status: "unknown",
+    },
+  ];
+}

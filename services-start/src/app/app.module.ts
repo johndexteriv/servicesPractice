edeb81b8a7b40320b387a0153da4d1eb7c039b07
John Garner <jdextergarneriv@gmail.com>
2021-03-08 15:47:06 -0500

    move AccountsService and LoggingService to app.module providers and restructue components accordingly

--- services-start/src/app/app.module.ts
+++ services-start/src/app/app.module.ts
@@ -1,23 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-
-import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
-import { NewAccountComponent } from './new-account/new-account.component';
+import { AppComponent } from "./app.component";
+import { AccountComponent } from "./account/account.component";
+import { NewAccountComponent } from "./new-account/new-account.component";
+import { AccountsService } from "./core/services/accounts/accounts.service";
+import { LoggingService } from "./core/services/logging/logging.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AccountComponent,
-    NewAccountComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, AccountComponent, NewAccountComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [AccountsService, LoggingService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
